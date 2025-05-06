b069ac2bf041e8fbe813a8f292af63ad2e763535
hsbbc <dacchomy1026@gmail.com>
2025-05-06 12:14:57 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -2,7 +2,7 @@ const userHeight = 1.7;
 const userWeight = 68;
 
 // 計算
-console.log(( userWeight % (userHeight * userHeight)).toFixed(2));
+console.log(( userWeight / (userHeight * userHeight)).toFixed(2));
 
 
   
